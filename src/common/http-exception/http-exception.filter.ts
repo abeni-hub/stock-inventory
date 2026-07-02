728532841d4952b2030bb5ee878758db68561acb
abeni-hub <ebenezersileshi@gmail.com>
2026-07-02 14:51:53 +0300

    updates

--- src/common/http-exception/http-exception.filter.ts
+++ src/common/http-exception/http-exception.filter.ts
@@ -1,6 +1,39 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
-@Catch()
-export class HttpExceptionFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+@Catch(HttpException)
+export class HttpExceptionFilter
+  implements ExceptionFilter
+{
+  catch(
+    exception: HttpException,
+    host: ArgumentsHost,
+  ) {
+    const ctx = host.switchToHttp();
+
+    const response =
+      ctx.getResponse<Response>();
+
+    const request =
+      ctx.getRequest<Request>();
+
+    const status =
+      exception.getStatus();
+
+    const error =
+      exception.getResponse();
+
+    response.status(status).json({
+      success: false,
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error,
+    });
+  }
 }

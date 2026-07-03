9e8bd64a53ef0f8079baff3c45ead9a3bfd22724
abeni-hub <ebenezersileshi@gmail.com>
2026-07-03 18:25:43 +0300

    finish exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/http-exception/http-exception.filter';
+import { LoggingInterceptor } from './common/logging/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.useGlobalFilters(
     new HttpExceptionFilter(),
 );
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
